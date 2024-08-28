c0a0cae552ff5439461da9c96370f0ef7d67c171
M. Andrés Piana <mateoapiana@gmail.com>
2024-08-28 17:41:23 -0300

    adding cors control

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 import express from 'express'
 import router from './routes/product.route.js'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:3000',
+  'http://localhost:5173'
+  // ! To add this the path of project-UI
+]
+
+const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+  origin: (origin, callback) => {
+    if (acceptedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+})
+
+app.use(corsMiddleware())
 
 app.use(express.json())
 
