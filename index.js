35f4b6385649dbebe314d2a08c97d3a712f1f570
M. Andrés Piana <mateoapiana@gmail.com>
2024-08-28 11:30:40 -0300

    Initial files of the project

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import router from './routes/product.route.js'
+import morgan from 'morgan'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+
+app.use(morgan('dev'))
+
+app.use('/product', router)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
